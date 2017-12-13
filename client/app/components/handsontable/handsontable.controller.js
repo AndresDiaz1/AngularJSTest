05c20665f16ae92422fe83f270188b6e11453b7e
AndresDiaz1 <andiazvar@gmail.com>
2017-12-12 20:07:04 -0500

    Added oninit method to handsontable component

--- client/app/components/handsontable/handsontable.controller.js
+++ client/app/components/handsontable/handsontable.controller.js
@@ -3,25 +3,25 @@ import Handsontable from 'handsontable-pro';
 class HandsontableController {
   constructor() {
     this.name = 'handsontable';
-    this.data = [
-      ['', 'Ford', 'Tesla', 'Toyota', 'Honda'],
+  }
+
+  $onInit() {
+    const tableHeader = ['Nombre', 'Email', 'Teléfono', 'Mensaje'];
+    const usersData = [
+      tableHeader,
       ['2017', 10, 11, 12, 13],
       ['2018', 20, 11, 14, 13],
       ['2019', 30, 15, 12, 13],
     ];
 
-    this.container = document.getElementById('usersTable');
-    this.hot = new Handsontable(this.container, {
-      data: this.data,
+    const container = document.getElementById('usersTable');
+    const usersTable = new Handsontable(container, {
+      data: usersData,
       rowHeaders: true,
       colHeaders: true,
       filters: true,
       dropdownMenu: true,
     });
-  }
-
-  $onInit() {
-    console.log('entro al on init');
   }
 }
 
