f3cf377ca67d2f90d974f569d4405f03d1cc3baf
AndresDiaz1 <andiazvar@gmail.com>
2017-12-12 21:59:25 -0500

    Testing scope inside arrow function

--- client/app/components/handsontable/handsontable.controller.js
+++ client/app/components/handsontable/handsontable.controller.js
@@ -9,9 +9,11 @@ class HandsontableController {
     };
     this.tableHeader = ['Nombre', 'Email', 'Teléfono', 'Mensaje'];
     this.thereAreUsers = false;
+    this.hasMadeSelection = false;
+    this.Idex=0;
   }
-
   $onInit() {
+    this.hasMadeSelection = false;
     if (this.services.dataHandler.thereAreUsers()) {
       this.thereAreUsers = true;
       this.createHandsonTable();
@@ -22,17 +24,31 @@ class HandsontableController {
     usersData.push(this.tableHeader);
     usersData = this.services.dataHandler.parseFromLocalstorageToHandsonTable(usersData);
     const container = document.getElementById('usersTable');
-    const usersTable = new Handsontable(container, {
+    let usersTable = new Handsontable(container, {
       data: usersData,
       rowHeaders: true,
       colHeaders: true,
       filters: true,
       dropdownMenu: true,
-      afterSelection: function(row,col) {
-        let data = this.getDataAtRow(row);
-        console.log('la data', data);
-      },
+      // afterSelection: (row)=> {
+      //   this.hasMadeSelection = true;
+      //   console.log("Selcciono", this.hasMadeSelection);
+      // //   let data = this.getDataAtRow(row);
+      // //   console.log('la data', data + ' y el indice', row);
+      //  },
     });
+
+    usersTable.addHook('afterSelection',(r)=>{
+      this.hasMadeSelection=true;
+      console.log("presiono", r);
+      this.llamar(r);
+    })
+  }
+  llamar(r){
+    this.hasMadeSelection=true;
+    console.log("eta lmamadno la funcion de afuera", r);
+    this.Idex=r;
+    console.log(this.Idex)
   }
 }
 
