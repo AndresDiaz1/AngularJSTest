46139356248975937b2b7f14faeee8db313f42d9
AndresDiaz1 <andiazvar@gmail.com>
2017-12-13 17:31:34 -0500

    Other unit test on service testing

--- client/app/services/datahandler/datahandler.service.spec.js
+++ client/app/services/datahandler/datahandler.service.spec.js
@@ -11,6 +11,12 @@ describe('DataHandler Service', () => {
     dataHandlerService = new DataHandler(localstorage);
   });
 
+  it('should push on users array new user when calls method saveUserInLocalStorage', () => {
+    dataHandlerService.users = [];
+    dataHandlerService.saveUserInLocalStorage({name: 'test', email: 'test@test', phone: 123, message: 'test message' });
+    expect(dataHandlerService.users.length).toEqual(1);
+  });
+
   it('should return selectedRow when calls method getSelectedRow', () => {
     dataHandlerService.selectedRow = 0;
     expect(dataHandlerService.getSelectedRow()).toEqual(0);
